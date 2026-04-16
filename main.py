5fb4ff2624e140f0d2eb53d6b4f395b0a940729d
SanjeeshwarS <sanjeeshwar@gmail.com>
2026-04-17 00:41:00 +0530

    Updated Code

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder
 
 df = pd.read_csv("loan_approval_1000.csv")
 
+print("===============\nDataset loaded successfully!\n=================")
 #data exploration
 print(df.head())
+print("===============================\nDataset info: null values\n===============================")
 print(df.isnull().sum())
 
+print("===============================\nCalculating mean and mode for missing values\n===============================")
 mean = df["Income"].mean()
 print("Mean Income:", mean)
 
@@ -16,4 +20,13 @@ print("Mode Employment Type : ", mode)
 df["Income"] = df["Income"].fillna(mean)
 df["Employment_Type"] = df["Employment_Type"].fillna(mode)
 
+print("===============================\nMissing values filled\n===============================")
+
+#checking for missing values after filling
 print(df.isnull().sum())
+
+print("===============================\nEncoding categorical variables\n===============================")
+#encoding categorical variables
+le = LabelEncoder()
+df['Employment_Type'] = le.fit_transform(df["Employment_Type"])
+print(le.classes_)
